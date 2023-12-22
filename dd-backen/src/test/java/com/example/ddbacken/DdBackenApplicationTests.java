0555eb67faecc1cda49922c9ca140f916afd9cff
Qianqi Li <qianqili@usc.edu>
2023-12-21 16:02:28 -0800

    initialized

---
+++ dd-backen/src/test/java/com/example/ddbacken/DdBackenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddbacken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdBackenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
