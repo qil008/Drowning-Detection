bebed4f1fed711f66e4f2ac55bde67fde8d5ef44
Qianqi Li <qianqili@usc.edu>
2023-12-27 23:23:06 -0800

    handle download

--- dd-fronten/src/components/Upload.vue
+++ dd-fronten/src/components/Upload.vue
@@ -3,19 +3,32 @@
         Choose File: <input type="file" ref="fileInputRef" @change="selectFile">
         <br />
         <button type="primary" @click="uploadFile">Upload</button>
+        <!-- Video player and download button will only show after a response of successful upload  -->
+        <!-- <video v-if="processedVideoUrl" controls width="320">
+            <source :src="processedVideoUrl" type="video/mp4">
+            Your browser does not support the video tag.
+        </video> -->
+        <button v-if="processedVideoUrl" @click="downloadVideo">Download Video</button>
     </div>
 </template>
-
+  
 <script>
 import axios from 'axios';
 
 export default {
     name: 'Upload',
+    data() {
+        return {
+            processedVideoUrl: '', // URL for the processed video
+            newVideoName: '', // The new name of the uploaded video (same as processed video)
+        }
+    },
     methods: {
         selectFile() {
             let file = this.$refs['fileInputRef'].files[0]
             if (file) {
                 console.log(`Selected file: ${file.name}`);
+                this.processedVideoUrl = ''; // hide download button when new video is selected and before upload completion.
             } else {
                 console.log("No file selected");
             }
@@ -44,15 +57,28 @@ export default {
                     alert(`Error: ${res.data.resCode}, Message: ${res.data.message}`);
                 } else {
                     alert('File uploaded successfully.');
+                    // Save the new video name and construct the URL for the processed video
+                    this.newVideoName = res.data.newVideoName;
+                    this.processedVideoUrl = `http://localhost:8080/video/download/${this.newVideoName}`;
                 }
             }).catch(error => {
                 alert(`Upload error: ${error.message}`);
             });
 
             this.$refs['fileInputRef'].value = ''
+        },
+        downloadVideo() {
+            // Use the HTML5 download attribute to download the video
+            const link = document.createElement('a');
+            link.href = this.processedVideoUrl;
+            link.download = this.newVideoName;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
         }
     }
 }
 </script>
-
+  
 <style></style>
+  
